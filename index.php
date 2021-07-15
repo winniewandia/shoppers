ee98ca0a75f561200f313adfdaf3467fe82d4c27
winniewandia <85342308+winniewandia@users.noreply.github.com>
2021-07-15 10:53:47 +0300

    logout code working

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ if (!isset($_SESSION['username'])) {
 	header('location: login.php');
 }
 
-if (isset($_POST['logout'])) {
+if (isset($_GET['logout'])) {
 	session_destroy();
 	unset($_SESSION['username']);
 	header('location: login.php');
@@ -34,7 +34,7 @@ if (isset($_POST['logout'])) {
 		<?php } ?>
 
 		<?php if (isset($_SESSION['username'])) { ?>
-			<p>Welcome<strong><?php echo $_SESSION['username']; ?> </strong></p>
+			<p>Welcome <strong><?php echo $_SESSION['username']; ?> </strong></p>
 			<p> <a href= "index.php?logout='1'" style="color: red;">logout</a></p>
 		<?php } ?>
 	</div>
